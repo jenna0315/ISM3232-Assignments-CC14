148362df59456976eefabf6bfc030a088d5aaf32
Your Name <you@example.com>
2024-11-03 22:49:39 -0500

    Task 4: Handle Errors and Use finally to Ensure Cleanup

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 //Task 2: Implement async/await to fetch ticket data and handle errors.
 async function fetchTicketData() {
+    try{
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     
     if (!response.ok) {
         throw new Error('Failed to fetch ticket');
     }
     
-    const tickets = await response.json();
+    const tickets = await response.json();}
+//Task 4: Handle Errors and Use finally to Ensure Cleanup
+    catch (error) {
+    console.error('Error:', error.message)
+} finally {
+    console.log('Fetch attempt completed.');
+}
+
 //Task 3: Dynamically display tickets on the page.
     tickets.forEach(ticket => {
     const ticketDiv = document.createElement('div');
@@ -17,5 +25,4 @@ async function fetchTicketData() {
         <p><strong>Issue Description:</strong> ${ticket.title}</p>
         <p><strong>Details:</strong> ${ticket.body}</p>
     `;})
-
-}
+    }
